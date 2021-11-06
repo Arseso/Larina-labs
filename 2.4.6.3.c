521d81b8ce0e5a92f992cdd9d3c012d414a2cb7c
Artem <92620390+Arseso@users.noreply.github.com>
2021-11-06 16:41:31 +0300

    laba 2, lvl B

--- 2.4.6.3.c
+++ 2.4.6.3.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 #include <malloc.h>
+#define max_len 21
 
 
 
 int main(void) {
     int i,j,g;
-    float **A, **B, **AB;
+    float **A, **B, **AB, **BE;
     int max_h;
     int temp, error;
 
 
 
     //init N
-    printf("\n I NEED YOUR N (0<N<=20): ");
+    printf("\n I NEED YOUR N (0<N<=%d): ", max_len-1);
         scanf("%d",&max_h);
-        if (max_h>0 && max_h<22) 
+        if (max_h>0 && max_h<max_len) 
         {
             A = (float**)malloc(sizeof(float)*max_h);
             AB = (float**)malloc(sizeof(float)*max_h);
+            BE = (float**)malloc(sizeof(float)*max_h);
             B = (float**)malloc(sizeof(float)*max_h);
             for (i = 0; i < max_h; i++) {
                 A[i] = (float*) malloc(max_h * sizeof(float));
                 AB[i] = (float*) malloc(max_h * sizeof(float));
+                BE[i] = (float*) malloc(max_h * sizeof(float));
                 B[i] = (float*) malloc(max_h * sizeof(float));
+
+                if(A == NULL || B == NULL || AB == NULL || BE == NULL){printf("NullPointerException, program stoped"); return 0;}
                 }
         } else {
             printf("\n INCORRECT N, PROGRAM STOPED");
@@ -79,22 +84,24 @@ int main(void) {
     //
     //
 
-    //B+E (matrix B)
+    //B+E (matrix BE)
 
     for(i=0; i<max_h; i++){
           for(j=0; j<max_h; j++){
             if(i==j){
-              B[i][j]+=1;
+              BE[i][j]+=B[i][j]+1;
+            } else {
+              BE[i][j]+=B[i][j];
             }
           }
         }
     
-    //A*B (matrix AB)
+    //A*BE (matrix AB)
 
     for (i = 0; i < max_h; i++) {
         for (j = 0; j< max_h; j++) {
             for (g = 0; g < max_h; g++) {
-                AB[i][j] += A[i][g] * B[g][j];
+                AB[i][j] += A[i][g] * BE[g][j];
             }
         }
     }
