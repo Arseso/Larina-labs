dbd5156a50973adf704f837a3e44d08f60b3648f
Artem <92620390+Arseso@users.noreply.github.com>
2021-12-27 21:14:00 +0300

    4 laba, lvl B

--- 3.8.7.c
+++ 3.8.7.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-char str[631];
+#include <string.h>
+#include <locale.h>
+#define max_words 30
+#define max_word 20
+#define ASCII_ZAGL_STROCH 32
+
 int get_len(char *string){
   int i = 0;
   while(string[i]!='\0'){
@@ -9,76 +14,68 @@ int get_len(char *string){
   return i;
 }
 
-void del_spaces(){
-  int i,k;
-  for (i = 0; str[i] != '\0'; i++)
-    if (str[i]==' ' || str[i]=='.' || str[i]=='!' || str[i]=='?'){
-      for (k = i; str[k] != '\0'; k++) str[k] = str[k+1]; 
-      i--;
- }
-}
 
-int is_correct(char *string){
-  int i, end, word_len, words_num, new_word;
-  i=word_len=end=words_num=0;
-
-  while(string[i]!= '\0'){
-    if(string[i]!=' '){
-      word_len++;
-    } else{
-      words_num++;
-      new_word = 1;
+int is_palin(char *input, int len){
+      int ok = 1;
+      int left = 0;
+      int right = len-1;
+      char b;
+        while (left < right)
+    {
+        while (input[left] == ' ' && left < right) left++;
+        while (input[right] == ' ' && right > left) right--; 
+        if (input[left] != input[right] && (input[left]+ASCII_ZAGL_STROCH) != input[right] && input[left] != (input[right]+ASCII_ZAGL_STROCH)) return 0; 
+        left++; right--;
     }
 
-    if(word_len>20 || words_num>30){
-      return 0;
-    }
-    
-    if(new_word){
-      word_len = 0;
-      new_word = 0;
-    }
-    
-    if(string[i]=='.' || string[i]=='!' || string[i]=='?'){
-      if(string[i+1]=='\0'){
-        end = 1;
-      } else { return 0;} 
-    }
-    i++;
-  }
-  if(end){
     return 1;
-  }
-  return 0;
 }
 
-int is_palin(char *string){
-  int k = get_len(string);
-  int i;
 
-  for(i=0; i<=k/2; i++){
-    if(!(string[i]==string[k-i-1])){
-      return 0;
-    }
-  }
-  return 1;
-}
 
 int main(void) {
-  int length,error;
+  int length, word_size, num_words, is_space, success_end;
+  word_size = num_words = success_end =0;
+  length = 1;
+  char *string, sym;
+  is_space = 0;
+  setlocale(LC_CTYPE, "rus");
+  if(!(string = (char*)malloc(length * sizeof(char)))) printf("NullPointer");
+  printf("\nВведите предложение: ");
 
+  while((sym=getchar()) != '\n'){
 
-  printf("Введите предложение: ");
-  gets(str);
-  length = get_len(str);
-  if(!is_correct(str)){
-    printf("Предложение введено неверно. Работа программы прекращена.");
-    return 0;
+    
+    if (!((( sym>= 'a' || sym >= 'A') && (sym <= 'z'|| sym <= 'Z')) || sym==' ' || sym=='?' || sym=='!' || sym=='.' )) {printf("Неизвестный символ. Программа остановена"); return 0;}
+    
+    length++;
+    if(!(string = (char*)realloc(string, sizeof(char)*(length)))) printf("NullPointer");
+    if(sym == '.' || sym == '?' || sym == '!'){
+      printf("\nВвод строки завершен"); 
+      success_end = 1;  
+      break;
+      }
+    
+    
+    if(sym == ' '){
+      if(!is_space){
+        num_words++;
+        string[length-2]=sym;
+        string[length-1]='\0';
+        is_space=1;
+        word_size=0;
+      }  else {length--;}
+    } else {
+      string[length-2]=sym;
+      string[length-1]='\0';
+      is_space=0; 
+      word_size++;}
+    
+    if(num_words>max_words || word_size>max_word){printf("\nПревышен размер предложения/cлова. Программа остановлена"); return 0;}
   }
- 
-  del_spaces();
-  
-  if(is_palin(str)){
+  if(!success_end){printf("\nПредложение должно заканчиваться соотвествующим знаком"); return 0;}
+
+  if(is_palin(string, length-2)){
     printf("\nПалиндром: True");
   } else {
     printf("\nПалиндром: False");
